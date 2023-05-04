f729b6b4c673b84f199380891d6ef2730f585b62
Damian Zienke <zienke.damian@gmail.com>
2023-05-04 19:37:19 +0200

    Add fetching new word from api
    New words used in game are now being fetched from `Supabase` API.
    
    Note:
    * the database contains 100 English words. therefore the new function `getWord` generates a random index between 1 and 100;
    * in case of modifying the number of records in the table, this part of the function needs to be updated as well

--- src/Ghostman.jsx
+++ src/Ghostman.jsx
@@ -1,4 +1,5 @@
 import React, {useState, useEffect, useMemo} from "react"
+import { createClient } from '@supabase/supabase-js'
 
 // React components
 import GameOn from "./components/Game-on"
@@ -6,7 +7,7 @@ import GameOff from "./components/Game-off"
 import GameStatus from "./components/Game-status"
 import Keyboard from "./components/Keyboard.jsx"
 
-import data from "./data"
+// import data from "./data"
 
 function Ghostman() {
   const [game, setGame] = useState({
@@ -15,21 +16,43 @@ function Ghostman() {
   })
   const [word, setWord] = useState("")
   const [guessedLetters, setGuessedLetters] = useState([])
-  const [chances, setChances] = useState(5)
+  const [chances, setChances] = useState(6)
   const [statusMsg, setStatusMsg] = useState("Good luck!")
   const wordArray = word.split("")
   const lettersToGuess = getLettersToGuess()
   const puzzle = getPuzzle()
 
   useEffect(()=>{
-    setWord(getWord())
+    getWord()
+      .then(data => {
+        setWord(data[0].puzzle_eng)
+      })
+      .catch(error => {
+        console.log(`Error: ${error}`)
+      })
   },[])
+  
+  async function getWord() {
+    const randomIndex = Math.floor(Math.random() * 100) + 1;
+    
+    const supabaseUrl = 'https://dtwbyxxwpwmlzdhjmcql.supabase.co'
+    const supabaseKey = import.meta.env.VITE_SUPABASE_KEY
+    const supabase = createClient(supabaseUrl, supabaseKey)
+    
+    let { data: randomWord, error } = await supabase
+    .from('puzzles-eng')
+    .select('puzzle_eng')
+    .eq('id', randomIndex)
 
-  function getWord() {
-    const wordsArray = data
-    const randomIndex = Math.floor(Math.random() * (data.length + 1));
-    return String(wordsArray[randomIndex].puzzle_eng)
+    return randomWord
   }
+
+  getWord()
+  // function getWord() {
+  //   const wordsArray = data
+  //   const randomIndex = Math.floor(Math.random() * (data.length + 1));
+  //   return String(wordsArray[randomIndex].puzzle_eng)
+  // }
 
   function getLettersToGuess() {
     return wordArray.filter((letter, index) => {
@@ -85,10 +108,16 @@ function Ghostman() {
   }
 
   function setNewGame() {
-    setWord(getWord())
-    toggleGame(true, "on")
-    setGuessedLetters([])
-    setChances(5)
+    getWord()
+    .then(data => {
+      setWord(data[0].puzzle_eng)
+      toggleGame(true, "on")
+      setGuessedLetters([])
+      setChances(6)
+    })
+    .catch(error => {
+      console.log(`Error: ${error}`)
+    })
   }
 
   function handleButtonClick() {
